8ccd3d06293de387e90b5ec28eec593a4738b2cc
Igor Savinkin <igor.savinkin@gmail.com>
2025-05-09 10:15:15 -0400

    handling events at form submission 2

--- SampleFolder/ProductSurvey/product_survey.js
+++ SampleFolder/ProductSurvey/product_survey.js
@@ -1,16 +1,25 @@
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
 
-const submitButton = document.getElementById ('submitBtn');
+
+
+const submitButton = document.getElementById('submitBtn');
 submitButton.onclick = submitFeedback;
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      submitFeedback();
+    }
+  });
 
 function submitFeedback() {
     alert('Thank you for your valuable feedback!');
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+    console.log("result:" + username + "  "+age+ "  "+email);
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -18,5 +27,5 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-
+    document.getElementById('userInfo').style.display = 'block';
 }
